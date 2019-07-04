70b19087111adf9a2f2c7293052c1cf436864b9b
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-07-04 15:35:35 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+TrimIndicators is based on code from Nereid's NanoGauges.
+
+NanoGauges used to have trim indicators, but they broke back around 1.3.1, and because of that Nereid removed them from the mod.
+
+This code is based on the old code, it has been updated to work with the current versions of KSP.
+
+I've offered to Nereid to readd these to his NanoGauges mod, but I wanted this as a standalone mod because this is only 6k in size, while the whole NanoGauges is over 260k in size, and I didn't want/need any of the other gauges
+
+Simply install into the GameData.  There are no configurable items
+
+This mod adds trim pointers to the three axis display during flight:  Roll, Yaw and Pitch.  The trim pointer is a smaller version of the pointer on the opposite side of the scale from the regular pointer
---
+++ src/TrimIndicators.cs
@@ -0,0 +1,113 @@
+﻿/*
+ * Trim Indicators based on an idea by dazoe 
+ * 
+ * */
+
+using System;
+using UnityEngine;
+using KSP.UI.Screens.Flight;
+
+namespace TrimIndicators
+{
+
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class TrimIndicatorsMono : MonoBehaviour
+    {
+        // the scaled size of the trim indicators
+        private const float SIZE = 0.75f;
+
+        // the new trim gauges
+        private KSP.UI.Screens.LinearGauge trimRollGauge;
+        private KSP.UI.Screens.LinearGauge trimPitchGauge;
+        private KSP.UI.Screens.LinearGauge trimYawGauge;
+
+        // the trim gauges get globally disabled if any exception is thrown
+        private static bool disabled = false;
+
+        private KSP.UI.Screens.LinearGauge CreateGauge(String name, KSP.UI.Screens.LinearGauge component)
+        {
+            GameObject indicator = (GameObject)MonoBehaviour.Instantiate(component.gameObject, Vector3.zero, Quaternion.identity);
+
+            indicator.name = name;
+            // set parent
+            indicator.transform.SetParent(component.transform.parent);
+            // set rotations
+            indicator.transform.rotation = component.transform.rotation;
+            indicator.transform.position = component.transform.position;
+            indicator.transform.localPosition = component.transform.localPosition;
+            indicator.transform.localScale = component.transform.localScale;
+            indicator.transform.localRotation = component.transform.localRotation;
+
+            KSP.UI.Screens.LinearGauge gauge = indicator.GetComponent<KSP.UI.Screens.LinearGauge>();
+            indicator.transform.localScale = Vector3.Scale(component.transform.localScale, new Vector3(-SIZE, SIZE, SIZE));
+
+            if (name == "GaugePitchTrimPointer")
+            {
+                // set size of the indicator by scaling them down and inverting the direction
+                float origWidth = gauge.pointer.rect.width / SIZE + 2;
+                indicator.transform.localPosition += new Vector3(gauge.pointer.rect.width, 0);
+
+                gauge.minValuePosition -= new Vector2(origWidth, 0);
+                gauge.maxValuePosition -= new Vector2(origWidth, 0);
+            }
+            else
+            {
+                // set size of the indicator by scaling them down and inverting the direction
+                float origHeight = gauge.pointer.rect.height + 2;
+
+                gauge.minValuePosition -= new Vector2(0, origHeight);
+                gauge.maxValuePosition -= new Vector2(0, origHeight);
+            }
+            //return gauge; 
+            return gauge;
+        }
+
+        public void Start()
+        {
+            // try to catch any exception if compatibility issues with KSP occur
+            try
+            {
+                var gaugesObject = (GameObject)MonoBehaviour.FindObjectOfType<LinearControlGauges>().gameObject;
+
+                var gaugesClass = gaugesObject.GetComponent<LinearControlGauges>();
+
+                KSP.UI.Screens.LinearGauge roll = gaugesClass.roll;
+                KSP.UI.Screens.LinearGauge pitch = gaugesClass.pitch;
+                KSP.UI.Screens.LinearGauge yaw = gaugesClass.yaw;
+
+                // create gauges
+                this.trimRollGauge = CreateGauge("GaugeRollTrimPointer", roll);
+                this.trimPitchGauge = CreateGauge("GaugePitchTrimPointer", pitch);
+                this.trimYawGauge = CreateGauge("GaugeYawTrimPointerw", yaw);
+            }
+            catch (Exception e)
+            {
+                Debug.Log("exception caught in trim indicator init");
+                disabled = true;
+                throw e;
+            }
+        }
+
+        public void Update()
+        {
+            if (disabled) return;
+            // try to catch any exception if compatibility issues with KSP occur
+            try
+            {
+                // update the trim gauges
+                FlightCtrlState ctrlState = FlightGlobals.ActiveVessel.ctrlState;
+
+                trimRollGauge.SetValue(ctrlState.rollTrim);
+                trimPitchGauge.SetValue(-ctrlState.pitchTrim);
+                trimYawGauge.SetValue(ctrlState.yawTrim);
+
+            }
+            catch
+            {
+                Debug.Log("exception caught in trim indicator update");
+                disabled = true;
+                Destroy(this);
+            }
+        }
+    }
+}
